9eac55772d328f1a2cc77bd033ace777512f485d
lsalatiel <lucassalatiel1004@gmail.com>
2024-01-28 16:33:23 -0300

    add button to change grid scale

--- src/script.js
+++ src/script.js
@@ -28,12 +28,53 @@ resetButton.addEventListener('click', () => {
     });
 });
 
-resetButton.addEventListener('mouseover', () => {
-    resetButton.style.backgroundColor="#585b70";
-    resetButton.style.color="white";
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('mouseover', () => {
+        button.style.backgroundColor="#585b70";
+        button.style.color="white";
+    });
 });
 
-resetButton.addEventListener('mouseout', () => {
-    resetButton.style.backgroundColor="#a6adc8";
-    resetButton.style.color="black";
+buttons.forEach((button) => {
+    button.addEventListener('mouseout', () => {
+        button.style.backgroundColor="#a6adc8";
+        button.style.color="black";
+    });
+});
+
+const gridButton = document.querySelector('#grid-btn');
+
+gridButton.addEventListener('click', () => {
+    let gridSize = prompt("Enter the grid size: ");
+    if(gridSize > 100 || gridSize < 1) {
+        alert("The grid size must be positive and less than 100");
+        return;
+    }
+
+    gridContainer.innerHTML = '';
+
+    for(let i = 0; i < gridSize*gridSize; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        // gridItem.textContent = i + 1;
+        gridContainer.appendChild(gridItem);
+        gridItem.style.backgroundColor="rgba(215, 215, 215, 1)";
+        gridItem.style.border="1px solid rgba(0, 0, 0, 0.8)";
+        gridItem.style.padding="20px";
+        gridItem.style.fontSize="30px";
+        gridItem.style.textAlign="center";
+    }
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+    // don't know why but this code needs to be here asswell
+    gridItens = document.querySelectorAll('.grid-item');
+
+    gridItens.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor="rgba(0, 0, 0, 0.8)";
+        });
+    });
 });
